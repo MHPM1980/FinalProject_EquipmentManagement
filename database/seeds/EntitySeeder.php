59feb7f6e711cc8bc90c7674024e21554d1eb215
Stawerr <renato-silva_16@hotmail.com>
2022-08-08 15:20:39 +0100

    Created Entity Model

---
+++ database/seeds/EntitySeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class EntitySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}

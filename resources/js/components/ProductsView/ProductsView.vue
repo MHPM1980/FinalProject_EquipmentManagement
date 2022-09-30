402941e2250f83c7bb5b9ac1eabe64baa0a3f059
TiagoMachado24 <tiago.mebm.24.07@gmail.com>
2022-09-30 22:30:07 +0100

    Edited permissions for equipmentView page

--- resources/js/components/ProductsView/ProductsView.vue
+++ resources/js/components/ProductsView/ProductsView.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="container">
-        <div class="row mt-3" v-if="$gate.isAdmin() || $gate.isGestor()">
+        <div class="row mt-3">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-header">
-                        <h3 class="card-title">Listagem de Equipamentos</h3>
+                        <div v-if="$gate.isFormando()" class="d-flex justify-content-start align-items-center">
+                            <h3 class="align-middle">Listagem de Equipamentos</h3>
+                            <h6 class="ml-2">(Para reservar contacte um Formador!)</h6>
+                        </div>
+                        <div v-else>
+                            <h3 class="card-title">Listagem de Equipamentos</h3>
+                        </div>
+
                         <div class="card-tools">
 
                         </div>
@@ -22,7 +29,7 @@
                                 <th class="text-center">Entidade</th>
                                 <th class="text-center">Armazém</th>
                                 <th class="text-center">Estado</th>
-                                <th class="text-center">Ação</th>
+                                <th v-if="!$gate.isFormando()" class="text-center">Ação</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -31,11 +38,11 @@
                                     <img class="imagem-equipamento" :src="'img/products/'+ product.image " alt="" title="">
                                 </td>
                                 <td class="align-middle text-center">{{ product.name }}</td>
-                                <td class="align-middle">{{ product.description }}</td>
-                                <td class="align-middle">{{ product.serial_number }}</td>
-                                <td class="align-middle">{{ product.category.name }}</td>
+                                <td class="align-middle text-center">{{ product.description }}</td>
+                                <td class="align-middle text-center">{{ product.serial_number }}</td>
+                                <td class="align-middle text-center">{{ product.category.name }}</td>
                                 <td class="align-middle text-center">{{ product.warehouse.entity_id }}</td>
-                                <td class="align-middle ">{{ product.warehouse.name }}</td>
+                                <td class="align-middle text-center">{{ product.warehouse.name }}</td>
                                 <td class="align-middle text-center">
                                     <div v-if="product.status == 1">
                                         <i  class="fa-solid fa-circle fa-lg fa-green"></i>
@@ -44,7 +51,7 @@
                                         <i  class="fa-solid fa-circle fa-lg fa-red"></i>
                                     </div>
                                 </td>
-                                <td class="align-middle text-center">
+                                <td v-if="!$gate.isFormando()" class="align-middle text-center">
                                     <a class="btn-primary btn" @click="listEquipment(product)">Detalhe</a>
 
 <!--                                   <a href="#" @click="editModal(product)">
@@ -64,12 +71,9 @@
                 </div>
             </div>
         </div>
-        <div v-if="!$gate.isAdmin() && !$gate.isGestor()">
-            <not-found></not-found>
+        <div v-if="$gate.isFormando()">
+
         </div>
-        <modal-product title="Reservar Equipamento">
-            <product-detail :edit-form="form"></product-detail>
-        </modal-product>
     </div>
 
 </template>
@@ -119,11 +123,9 @@
                 this.form.fill(product);
             },
             loadProducts(){
-                if(this.$gate.isAdmin() || this.$gate.isGestor()){
                 axios
                     .get("api/products/")
                     .then(({ data }) => (this.products = data))
-                };
             },
         }
     }

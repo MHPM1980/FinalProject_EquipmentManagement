231272e3e606654e3c3a0c81e6428a5c52f08d38
Stawerr <renato-silva_16@hotmail.com>
2022-10-27 19:00:05 +0100

    clean seeds and make refactor on destroy function at productController to delete image when we delete a product

--- database/seeds/EntitySeeder.php
+++ database/seeds/EntitySeeder.php
@@ -11,7 +11,6 @@ class EntitySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('entities')->insert(['name' => 'ATEC','address' => 'Rua Pinheiro Bravo 276','phone_number'=>'226587452','created_at'=>now()]);
-        DB::table('entities')->insert(['name' => 'Siemens','address' => 'Rua de Cima 172','phone_number'=>'22568974','created_at'=>now()]);
+
     }
 }

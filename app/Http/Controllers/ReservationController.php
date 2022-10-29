30fe88fdd7fcbc4a14ab730f7fcf74c14e631e3c
Marcio Miguel <mscanja@gmail.com>
2022-10-29 22:27:35 +0100

    added search function to reservation - finish

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -48,34 +48,42 @@ class ReservationController extends Controller
     public function search(Request $request){
         if($search= \Request::get('q') && Auth::user()->role->name=='FORMADOR'){
             $search = $request->q;
-            return DB::table('reservations')
-                ->join('users','reservations.user_id', '=' ,'users.id')
-                ->join('products', 'reservations.product_id', '=', 'products.id')
-                ->join('warehouses', 'reservations.warehouse_id', '=', 'warehouses.id')
-                ->join('entities', 'warehouses.entity_id', '=', 'entities.id')
-                ->where("user_id", "=", Auth::user()->id)
-                ->where('users.name','like',"%$search%")
-                ->orWhere('products.name','LIKE',"%$search%")
-                ->orWhere('warehouses.name','LIKE',"%$search%")
-                ->orWhere('entities.name','LIKE',"%$search%")
-                ->orderByDesc('reservations.registry_date')
+            return Reservation::with(['user','product','warehouse.entity'])
+                ->whereHas('user', function($query) use ($search){
+                    $query->where('name','LIKE',"%$search%");
+                })
+                ->orWhereHas('product', function($query) use ($search){
+                    $query->where('name','LIKE',"%$search%");
+                })
+                ->orWhereHas('warehouse', function($query) use ($search){
+                    $query
+                        ->where('name','LIKE',"%$search%")
+                        ->orWhereHas('entity', function($query) use ($search){
+                            $query->where('name','LIKE',"%$search%");
+                        });
+                })
                 ->paginate(9);
 
 
 
         }else if($search= \Request::get('q') && Auth::user()->role->name!=='FORMADOR'){
             $search = $request->q;
-            return DB::table('reservations')
-                ->join('users','reservations.user_id', '=' ,'users.id')
-                ->join('products', 'reservations.product_id', '=', 'products.id')
-                ->join('warehouses', 'reservations.warehouse_id', '=', 'warehouses.id')
-                ->join('entities', 'warehouses.entity_id', '=', 'entities.id')
-                ->where('users.name','like',"%$search%")
-                ->orWhere('products.name','LIKE',"%$search%")
-                ->orWhere('warehouses.name','LIKE',"%$search%")
-                ->orWhere('entities.name','LIKE',"%$search%")
-                ->orderByDesc('reservations.registry_date')
+            return Reservation::with(['user','product','warehouse.entity'])
+                ->whereHas('user', function($query) use ($search){
+                    $query->where('name','LIKE',"%$search%");
+                })
+                ->orWhereHas('product', function($query) use ($search){
+                    $query->where('name','LIKE',"%$search%");
+                })
+                ->orWhereHas('warehouse', function($query) use ($search){
+                    $query
+                        ->where('name','LIKE',"%$search%")
+                        ->orWhereHas('entity', function($query) use ($search){
+                            $query->where('name','LIKE',"%$search%");
+                        });
+                })
                 ->paginate(9);
+
 
 
         } else if(Auth::user()->role->name=='FORMADOR'){
